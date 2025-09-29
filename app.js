086d4c1a175cf0438942dbd26627e3679db91f1a
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-29 10:24:56 -0400

    feat: parse log in info and simply validate non-empty field

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 const app = express()
 const mainRouter = require('./routes/main')
 
+app.use(express.json())
 app.use('/api/v1', mainRouter)
 
 app.use(notFoundMiddleware)
--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,17 @@
+const CustomAPIError = require("../errors/custom-error")
+
 const login = async (req, res) => {
-  console.log(`log in`)
-  res.send('log in')
+  const { email, password } = req.body
+  console.log(email, password)
+  if (!email || !password){
+    throw new CustomAPIError('Please provide email and password to proceed', 400)
+  }
+  res.send('Log in coming ...')
 }
 
-const dashboard = async(req, res) => {
+const dashboard = async (req, res) => {
   console.log(`dashboard`)
   res.send('dashboard')
 }
 
-module.exports = {login, dashboard}
+module.exports = { login, dashboard }
