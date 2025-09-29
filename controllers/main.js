13ef33a41b8cfa8b4b6f9d47f6b5bf74f41648c7
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-29 17:44:54 -0400

    feat: jwt set up for login controller

--- controllers/main.js
+++ controllers/main.js
@@ -1,12 +1,20 @@
+const jwt = require("jsonwebtoken")
 const CustomAPIError = require("../errors/custom-error")
 
 const login = async (req, res) => {
   const { email, password } = req.body
   console.log(email, password)
-  if (!email || !password){
+  if (!email || !password) {
     throw new CustomAPIError('Please provide email and password to proceed', 400)
   }
-  res.send('Log in coming ...')
+  const payload = {
+    id: new Date().getDate,
+    email: email
+  }
+
+  const privateKey = process.env.JWT_PRIVATE_KEY
+  const token = jwt.sign(payload, privateKey, { algorithm: 'HS256', expiresIn: '20m' })
+  res.status(200).json({ ...payload, token: token })
 }
 
 const dashboard = async (req, res) => {
