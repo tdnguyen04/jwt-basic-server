15771f196971e55a19f3764055e9e3d36a6ef863
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-30 17:13:19 -0400

    add: plan out the basic logic for controllers regarding interaction with database

--- controllers/main.js
+++ controllers/main.js
@@ -4,17 +4,34 @@ const CustomAPIError = require("../errors/custom-error")
 const login = async (req, res) => {
   const { email, password } = req.body
   console.log(email, password)
+  // input validation
   if (!email || !password) {
     throw new CustomAPIError('Please provide email and password to proceed', 400)
   }
+
+  // get data
   const payload = {
     id: new Date().getDate,
     email: email
   }
 
+  // create JWT token
   const privateKey = process.env.JWT_PRIVATE_KEY
   const token = jwt.sign(payload, privateKey, { algorithm: 'HS256', expiresIn: '20m' })
+
+  // respond to client
   res.status(200).json({ ...payload, token: token })
+}
+
+const signup = async (req, res) => {
+  const {email, password, firstName, lastName} = req.body
+  // input validation
+  if (!email || !password || !firstName || !lastName) {
+    throw new CustomAPIError('Please provide email and password to proceed', 400)
+  }
+  // update data
+
+  // respond to client
 }
 
 const dashboard = async (req, res) => {
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { login, dashboard } = require('../controllers/main')
+const { login, dashboard, signup } = require('../controllers/main')
 const router = express.Router()
 
 router.route('/login').post(login)
 router.route('/dashboard').get(dashboard)
+router.route('/signup').post(signup)
 
 module.exports = router
